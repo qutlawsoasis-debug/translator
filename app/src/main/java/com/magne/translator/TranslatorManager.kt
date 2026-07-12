a9f29c4d8a3646335661b5c048fb3275b042a25c
qutlawsoasis-debug <qutlawsoasis@gmail.com>
2026-07-12 03:45:52 +0200

    Fix ML Kit continuous download bug by checking model existence first

--- app/src/main/java/com/magne/translator/TranslatorManager.kt
+++ app/src/main/java/com/magne/translator/TranslatorManager.kt
@@ -14,11 +14,21 @@ class TranslatorManager {
     private val translator = Translation.getClient(options)
 
     fun downloadModelIfNeeded(onSuccess: () -> Unit, onError: (Exception) -> Unit) {
-        val conditions = DownloadConditions.Builder()
-            .build()
-        translator.downloadModelIfNeeded(conditions)
-            .addOnSuccessListener { onSuccess() }
-            .addOnFailureListener { onError(it) }
+        val modelManager = com.google.mlkit.common.model.RemoteModelManager.getInstance()
+        val model = com.google.mlkit.nl.translate.TranslateRemoteModel.Builder(TranslateLanguage.RUSSIAN).build()
+        
+        modelManager.isModelDownloaded(model).addOnSuccessListener { isDownloaded ->
+            if (isDownloaded) {
+                onSuccess()
+            } else {
+                val conditions = DownloadConditions.Builder().build()
+                translator.downloadModelIfNeeded(conditions)
+                    .addOnSuccessListener { onSuccess() }
+                    .addOnFailureListener { onError(it) }
+            }
+        }.addOnFailureListener { 
+            onError(it)
+        }
     }
 
     fun translate(russianText: String, onSuccess: (String) -> Unit, onError: (Exception) -> Unit) {
